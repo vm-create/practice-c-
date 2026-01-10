aaa4764ba36f6e4fa100206241f6b15a5776eb7f
vm-create <vaibhav2007vm@gmail.com>
2026-01-11 00:48:35 +0530

    code till video 2

---
+++ dsa.cpp
@@ -0,0 +1,185 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int age = 25;
+//     cout << sizeof(age) << endl;
+//     return 0;
+// }
+//  boiler plate code
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int age = 25;
+//     char grade = 'a';
+//     char name = 'b';
+//     float pi = 3.14f;
+//     bool isSAFE = true;
+
+//     cout << age << grade << name << pi << isSAFE << endl; // true-> 1 & false->0
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char grade = 'a'; // ascii value of a is 97 so character a is stored as integer valur 97
+//     int value = grade; // typecasting
+//     cout << value << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     double price = 100.99;
+//     int value = (int)price;
+//     cout << value << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout << "enter your age: ";
+//     cin >> age;
+//     cout << "your age is "
+//          << age << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 30, b = 6;
+
+//     cout << "sum=" << (a + b);
+//     cout << "difference =" << (a - b) << endl;
+//     cout << "product =" << (a * b) << endl;
+//     cout << "division=" << (a / b) << endl;
+//     cout << "modulo =" << (a % b) << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 5, b = 2;
+//     int ans = (a / (float)b);
+//     cout << "division= " << ans << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << (3 < 5) << endl;  // true=1
+//     cout << (3 > 5) << endl;  // false=0
+//     cout << (3 <= 3) << endl; // true=1
+//     cout << (3 == 3) << endl; // true=1
+//     cout << (3 == 5) << endl; // false=0
+//     cout << (3 != 5) << endl; // true=1
+//     cout << (3 != 3) << endl; // false=0
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << (3 > 1) << endl;  // true=1
+//     cout << !(3 > 1) << endl; // ! is not operator which converts true to false so ans is 0
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << ((3 > 5) || (3 < 5)) << endl; // or operator is used and one statement is true thus the ans is true
+//     cout << ((6 > 5) && (3 > 2)) << endl; // and operator gives true(1) as both are true
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a, b;
+//     cout << "enter 1st number:";
+//     cin >> a;
+//     cout << "enter 2nd number:";
+//     cin >> b;
+//     cout << "sum=" << (a + b) << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 10;
+//     int b = a++;
+//     cout << "b=" << b << endl; // kaam matlab assign ho gyi value
+//     cout << "a=" << a << endl; // upadte
+//     cout << "b=" << b << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 10;
+//     int b = ++a;
+//     cout << "b=" << b << endl;
+//     cout << "a=" << a << endl;
+//     cout << "b=" << b << endl;
+
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 10;
+//     int b = --a;
+//     cout << "b=" << b << endl;
+//     cout << "a=" << a << endl;
+//     cout << "b=" << b << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a, b;
+//     cout << "enter 1st number=";
+//     cin >> a;
+//     cout << "enter 2nd number=";
+//     cin >> b;
+//     cout << "sum =" << (a + b) << endl
+//          << "difference =" << (a - b) << endl
+//          << "product= " << (a * b) << endl
+//          << "division= " << (a / b) << endl
+//          << "modulo = " << (a % b) << endl;
+//     return 0;
+// }
